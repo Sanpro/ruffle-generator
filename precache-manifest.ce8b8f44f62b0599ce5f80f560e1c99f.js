561846c006ad1336d03a5141f765149830a361a0
Sanpro <prozid@gmail.com>
2024-10-01 23:36:07 +0000

    Updates

---
+++ precache-manifest.ce8b8f44f62b0599ce5f80f560e1c99f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae84a560340e5e779c7",
+    "url": "/ruffle-generator/static/css/main.26f5fcc4.chunk.css"
+  },
+  {
+    "revision": "3ae84a560340e5e779c7",
+    "url": "/ruffle-generator/static/js/main.3ae84a56.chunk.js"
+  },
+  {
+    "revision": "95408cc01bb1694f14e0",
+    "url": "/ruffle-generator/static/js/runtime~main.95408cc0.js"
+  },
+  {
+    "revision": "1b84ef05a014670d5093",
+    "url": "/ruffle-generator/static/js/2.1b84ef05.chunk.js"
+  },
+  {
+    "revision": "8e9263ed3107c96749b3a6e94a83c0b5",
+    "url": "/ruffle-generator/index.html"
+  }
+];
